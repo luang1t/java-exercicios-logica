4da0e44be7d463510e3c8f67a5b47bb1e1e7b96d
luang1t <luanzragit@gmail.com>
2025-07-03 02:06:42 -0300

    Novos exercícios resolvidos

---
+++ src/Exercicio04_LevelUpLoop.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+/**
+ * Exercicio 4 - Loop de Experiência
+ * 
+ * Peça ao jogador o nível atual e mostre a progressão de níveis até o nível 18.
+ * Use um laço de repetição para exibir cada nível subsequente.
+ * 
+ * Entrada:
+ * - Nível atual do jogador (int)
+ * 
+ * Saída:
+ * - Lista de níveis até 18 (int)
+ */
+
+public class Exercicio04_LevelUpLoop {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int nivelAtual, i;
+
+        System.out.println("Digite seu nível atual: ");
+        nivelAtual = scanner.nextInt();
+
+        if (nivelAtual >= 1 && nivelAtual <= 18) {
+            System.out.println("Evoluindo do nível " + nivelAtual + " até o nível 18.");
+
+            for (i = nivelAtual; i <= 18; i++) {
+                System.out.println("Nível subindo: " + i);
+            }
+        } else
+            System.out.println("Digite um nível entre 1 e 18.");
+    }
+}
---
+++ src/Exercicio05_DamageCalculator.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+/**
+ * Exercicio 5 - Calculadora de Dano
+ * 
+ * Peça ao jogador o dano base de uma habilidade e o multiplicador de poder de
+ * habilidade (AP).
+ * Calcule o dano final usando a fórmula:
+ * 
+ * danoFinal = danoBase + (AP * multiplicador)
+ * 
+ * Entrada:
+ * - Dano base (double)
+ * - Poder de habilidade (double)
+ * - Multiplicador (double)
+ * 
+ * Saída:
+ * - Dano final (double)
+ */
+
+public class Exercicio05_DamageCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double danoBase, poderDeHabilidade, multiplicador, danoFinal;
+
+        System.out.println("Digite o dano base do seu personagem: ");
+        danoBase = scanner.nextDouble();
+
+        System.out.println("Digite o quanto de poder de habilidade voce conseguiu com itens: ");
+        poderDeHabilidade = scanner.nextDouble();
+
+        System.out.println("Digite o multiplicador de dano que seu personagem possuí (ex: 0.6 para 60%): ");
+        multiplicador = scanner.nextDouble();
+
+        danoFinal = danoBase + (poderDeHabilidade * multiplicador);
+        System.out.println("Voce está com um total de: " + danoFinal + " poder de habilidade 'AP'.");
+
+    }
+}
